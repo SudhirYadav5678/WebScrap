d8dd039e92af172b14768305dee523e276cbf7c9
SudhirYadav <sudhirkyadav51@gmail.com>
2025-05-27 15:18:24 +0530

    index

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,16 +3,16 @@ import { app } from "./app.js"
 import { dbConnect } from "./utiles/dbConnection.js"
 
 
+// Load environment variables
 dotenv.config({
     path: './.env'
 })
 
-dbConnect()
-    .then(() => {
-        app.listen(process.env.PORT || 5000, () => {
-            console.log(`Server is running at port : ${process.env.PORT}`);
-        })
-    })
-    .catch((err) => {
-        console.log("Server connection failed !!! ", err);
-    })
+dbConnect().catch((err) => {
+    console.log("Server connection failed !!! ", err);
+});
+
+// For Vercel: export the Express app as default (do not call listen)
+export default app;
+
+// For local development, you can use a separate script to call app.listen if needed.
