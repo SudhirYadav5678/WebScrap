0ec0e8199f065ae1294104a7f6cabaf38bed3cec
SudhirYadav <sudhirkyadav51@gmail.com>
2025-05-28 00:18:46 +0530

    scrap change

--- scrap/scrap.py
+++ scrap/scrap.py
@@ -8,13 +8,30 @@ headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleW
 
 
 
-def fetchAndWrite(url: str,path):
+# def fetchAndWrite(url: str,path):
+#     try:
+#         r = requests.get(url)
+#         with open(path,"w") as f:
+#             f.write(r.text) # Raise an error for bad responses
+#         print(f"✅ HTML fetched and written to {path}")
+#         return True
+#     except requests.RequestException as e:
+#         print(f"❌ Error fetching {url}: {e}")
+#         return False
+
+def fetchAndWrite(url: str, path: str):
     try:
-        r = requests.get(url)
-        with open(path,"w") as f:
-            f.write(r.text) # Raise an error for bad responses
+        # Ensure the directory exists
+        os.makedirs(os.path.dirname(path), exist_ok=True)
+
+        r = requests.get(url,timeout=10, headers=headers)
+        with open(path, "w") as f:
+            f.write(r.text)
         print(f"✅ HTML fetched and written to {path}")
         return True
     except requests.RequestException as e:
         print(f"❌ Error fetching {url}: {e}")
+        return False
+    except Exception as e:
+        print(f"❌ General error: {e}")
         return False
